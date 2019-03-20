e20e2262d28a0927760486c8f7731c56f04fd7c3
smehrazi <47094882+smehrazi@users.noreply.github.com>
2019-03-20 09:13:43 -0700

    Update annual.stats.onlySanSanta.R
    I edited mostly line4.

--- annual.stats.onlySanSanta.R
+++ annual.stats.onlySanSanta.R
@@ -1,6 +1,6 @@
 annual.stats.onlySanSanta <- function(x){
-  b <- filter(x, str_detect(`Site Name`, "San\\s|Santa")) #extracting any 'San' or 'Santa' from the original dataset
+  b <- filter(x, str_detect(`Site Name`, "Santa|San")) #extracting any 'San' or 'Santa' from the original dataset
   b %>%
-    group_by(Year = str_sub(as.factor(date), start = 1, end = 4), `Site Name`, site) %>%
+    group_by(year(date)) %>% # Here you do not need to define it with a long previous line you had
     summarize(o3.mean = mean(o3, na.rm = T), o3.median = median(o3, na.rm = T), o3.max = max(o3, na.rm = T), o3.min = min(o3, na.rm = T))
 }
